79b65e2b9b09c632464f56dbdfe4a42ecdb8774f
Joseph Eiba <josepheiba@icloud.com>
2024-05-17 20:39:54 +0100

    ok

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+hello Flask!
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """
+    hello HBNB
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def widlcard(text):
+    """
+    Widlcard usage
+    """
+    return f"{text}"
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
